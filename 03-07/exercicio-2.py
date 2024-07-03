eb9c53e07cd8a29876a6dfb45cf822b8cebec9fd
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-03 17:00:17 -0300

    Create exercicio-2.py

---
+++ 03-07/exercicio-2.py
@@ -0,0 +1,8 @@
+#Exercício 2:  Imprima o valor de (x + y)*z.
+#Lembrando que o operador * é multiplicação.
+
+x = int(input("Informe o valor para x: "))
+y = int(input("Informe o valor para y: "))
+z = int(input("Informe o valor para z: "))
+
+print((x + y)*z)
