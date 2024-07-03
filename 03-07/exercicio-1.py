5273a06b3b15d16a862d98b07fc325dc53086cdf
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-03 16:58:31 -0300

    Create exercicio-1.py

---
+++ 03-07/exercicio-1.py
@@ -0,0 +1,5 @@
+#Exercício 1:  Use a função 'input' para atribuir 3 números inteiros à 3 variáveis x, y e z.
+
+x = int(input("Informe o valor para x: "))
+y = int(input("Informe o valor para y: "))
+z = int(input("Informe o valor para z: "))
