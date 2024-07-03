e11a2e2dc75bc29200909e89d8d84b8ed51b0a61
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-03 16:53:28 -0300

    Create troca-variaveis.py

---
+++ 03-07/troca-variaveis.py
@@ -0,0 +1,13 @@
+#Programa que troca o valor de duas variáveis, usando uma variável temporária.
+
+#Variáveis
+var1 = 0
+var2 = 1
+
+#Algoritmo de 3 linhas.
+temp = var1
+var1 = var2
+var2 = temp
+
+#Verificação de corretude:
+print(var1, var2)
