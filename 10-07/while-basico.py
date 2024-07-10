c7b611832f9522f7c1dc611e3919f2953643f058
Judismar Arpini Junior <solid.judis@gmail.com>
2024-07-10 08:49:19 -0300

    Create while-basico.py

---
+++ 10-07/while-basico.py
@@ -0,0 +1,19 @@
+from random import randint
+
+#'cara' é representado por 1
+#'coroa' é representado por 2
+#Verdadeiro: True
+#Falso: False
+repetir = True
+while repetir:
+    escolha = input("Escolha entre cara ou coroa: ")
+    resultado = randint(1, 2)
+    if resultado == 1:
+        resultado = "cara"
+    else:
+        resultado = "coroa"
+    print("Resultado:", resultado)
+    if escolha == resultado:
+        print("Você venceu!")
+    else:
+        print("Você perdeu...")
