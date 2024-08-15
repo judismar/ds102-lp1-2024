b680ac5a1aa756b751115ffc3561fa2df93c2498
Judismar Arpini Junior <solid.judis@gmail.com>
2024-08-15 12:42:07 -0300

    Add files via upload

---
+++ 16-08/robo.py
@@ -0,0 +1,23 @@
+from mundo import printm
+
+def efetua_passo(pos, mundo, direcao, passo):
+	if passo <= 0:
+		return
+	elif direcao == "esquerda" or direcao == "e":
+		passo *= -1
+	elif direcao == "direita" or direcao == "d":
+		pass
+	else:
+		passo *= -1
+	n = len(mundo)
+	printm(pos, mundo, -1)
+	for _ in range(abs(passo)): #repete 'passo' vezes
+		k = (1 if passo > 0 else -1)
+		if pos + k < 0 or pos + k == n:
+			return
+#		if mundo[pos + k] == 1:
+#			printm(pos, mundo, pos+k)
+#			return
+		else:
+			pos += k
+		printm(pos, mundo, -1)
